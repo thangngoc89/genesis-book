ebb19645c863eff56817fafe9007008ebb53f577
Khoa Nguyen <khoa02031995@gmail.com>
2015-10-25 02:39:46 +0700

    Edit home page grid styles

---
+++ home.php
@@ -0,0 +1,48 @@
+<?php
+
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+
+/**
+ * Display as Columns
+ *
+ */
+function be_article_post_class( $classes ) {
+
+	global $wp_query;
+	if( !$wp_query->is_main_query() )
+		return $classes;
+
+	$columns = 4;
+
+	$column_classes = array( '', '', 'one-half', 'one-third', 'one-fourth', 'one-fifth', 'one-sixth' );
+	$classes[] = $column_classes[$columns];
+	if( 0 == $wp_query->current_post || 0 == $wp_query->current_post % $columns )
+		$classes[] = 'first';
+
+	return $classes;
+}
+add_filter( 'post_class', 'be_article_post_class' );
+// Remove items from loop
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
+remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
+remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
+/**
+ * Add article Image
+ *
+ */
+function be_article_image() {
+	echo wpautop( '<a href="' . get_permalink() . '">' . genesis_get_image( array( 'size' => 'medium' ) ). '</a>' );
+}
+add_action( 'genesis_entry_content', 'be_article_image' );
+add_filter( 'genesis_pre_get_option_content_archive_thumbnail', '__return_false' );
+// Move Title below Image
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+add_action( 'genesis_entry_footer', 'genesis_entry_header_markup_open', 5 );
+add_action( 'genesis_entry_footer', 'genesis_entry_header_markup_close', 15 );
+add_action( 'genesis_entry_footer', 'genesis_do_post_title' );
+
+genesis();
